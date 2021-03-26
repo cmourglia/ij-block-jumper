05909111be5121825574d9e9cb604803155afc31
GitHub Action <action@github.com>
2021-03-26 10:57:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/cmourglia/ijblockjumper/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/cmourglia/ijblockjumper/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.cmourglia.ijblockjumper.listeners
 
+import com.github.cmourglia.ijblockjumper.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
